e3603d6529f5c095d900d2cbec5c25705fa52aa7
Kishan <kishanhs1990@gmail.com>
2020-10-23 16:35:37 +0530

    useState with custom hooks

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,7 +1,13 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
   return (
     <div className="search-params">
       <form>
@@ -16,6 +22,8 @@ const SearchParams = () => {
             }}
           />
         </label>
+        <AnimalDropdown />
+        <BreedDropdown />
         <button>Submit</button>
       </form>
     </div>
